e073eb30be45ad12ea6e22e899ec68675b951077
Kundan Sagar <118827781+kundansagarr8@users.noreply.github.com>
2024-04-14 13:26:24 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+
+var i = document.querySelector("i");
+var a = document.querySelector(".goto-top");
+
+
+a.addEventListener("click",function(){
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+})
+
